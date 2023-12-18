6047a236e62dce5fc777a1baddfbe45f42f8b201
Nate Vaughan <nate.vaughan@protonmail.com>
2023-12-18 14:10:48 -0600

    mutable borrows?

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,29 +1,7 @@
 fn main() {
-    println!("Don't forget to add semicolons to anything that is not a return statement in Rust!");
     let mut a = 10;
-    println!("Let's do some addition. Let a = {a}");
-    let b = a;
-    a = 11;
-    println!("Now let b = a and set a = 11");
-    println!("b = {b}");
-    println!("a = {a}");
-    let mut c = Container { val: a };
-    println!("Now wrap it in a container called Container and assign to c: ");
-    let d = c;
-    c.val = 12;
-    println!("Just for fun we will set d = c and change c's value");
-    println!("c.val = {}", c.val);
-    println!("d.val = {}", d.val);   
-}
-
-struct Container {
-    val: i64
-}
-
-impl Copy for Container { }
-
-impl Clone for Container {
-    fn clone(&self) -> Container {
-        *self
-    }
+    let ref mut b = a;
+    *b += 1;
+    // does not compile due to mutable borrow
+    println!("{b} {a}")
 }
