ad71d36cdcbb20995680501abc695c76ba4d5609
Nate Vaughan <nate.vaughan@protonmail.com>
2023-12-18 13:34:27 -0600

    Playing with references, clones, dereferencing

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,16 +1,29 @@
 fn main() {
-    use Bing::FOO;
-
-    let scoped = "is this scope shadowed!?".to_string();
-    {
-        let scoped = "YES!".to_string();
-        println!("{}", scoped
-    )
-    }
-    println!("{}", scoped);
-    println!("{}", FOO)
+    println!("Don't forget to add semicolons to anything that is not a return statement in Rust!");
+    let mut a = 10;
+    println!("Let's do some addition. Let a = {a}");
+    let b = a;
+    a = 11;
+    println!("Now let b = a and set a = 11");
+    println!("b = {b}");
+    println!("a = {a}");
+    let mut c = Container { val: a };
+    println!("Now wrap it in a container called Container and assign to c: ");
+    let d = c;
+    c.val = 12;
+    println!("Just for fun we will set d = c and change c's value");
+    println!("c.val = {}", c.val);
+    println!("d.val = {}", d.val);   
 }
 
-mod Bing {
-    pub static FOO: &str = "exported value!!";
+struct Container {
+    val: i64
+}
+
+impl Copy for Container { }
+
+impl Clone for Container {
+    fn clone(&self) -> Container {
+        *self
+    }
 }
