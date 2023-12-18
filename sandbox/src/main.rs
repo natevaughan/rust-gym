10deabbfedf9571209e72a036ef040b3b56c7d91
Nate Vaughan <nate.vaughan@protonmail.com>
2023-12-18 12:03:32 -0600

    Sandbox w/ strings and scoping

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,29 +1,16 @@
 fn main() {
-    fn double(num: u128) -> u128 {
-      num * 2
+    use Bing::FOO;
+
+    let scoped = "is this scope shadowed!?".to_string();
+    {
+        let scoped = "YES!".to_string();
+        println!("{}", scoped
+    )
     }
-  
-    let int: i32 = 32;
-    let big_int = 10;
-    let float = 1.2;
-  
-    let outcome = double(big_int);
-    let int2 = double(int.try_into().unwrap());
-    let doubled_outcome = outcome;
-  
-    println!("{doubled_outcome}");
-  
-    println!("{int2} {float}");
+    println!("{}", scoped);
+    println!("{}", FOO)
+}
 
-    println!("Let's try a statement");
-
-    let foo = {
-        let bar = f64::from(doubled_outcome as u32) * float;
-        println!("({doubled_outcome} * {float}) + {int2} = ");
-        bar as u64 + u64::try_from(int2).unwrap_or(0)
-    };
-
-    println!("{foo}")
-    
-  }
-  
+mod Bing {
+    pub static FOO: &str = "exported value!!";
+}
