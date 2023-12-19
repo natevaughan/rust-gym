1051339d2e007eb48e8710e0d416cdf4f72d8295
Nate Vaughan <nate.vaughan@protonmail.com>
2023-12-18 21:35:22 -0600

    Function that performs reverse

--- string_word_reverse/src/main.rs
+++ string_word_reverse/src/main.rs
@@ -1,11 +1,18 @@
 use std::env;
 
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     println!("original arguments: {}", args.join(" "));
-    let words = &mut args[1..];
+    let words: Vec<String> = args[1..].iter().map(|x| x.to_string()).collect();
     println!("word arguments: {}", words.join(" "));
-    words.reverse();
-    println!("reversed arguments: {}", words.join(" "));
+    let reversed = reverse(&words);
+    println!("reversed arguments: {}", reversed.join(" "));
     println!("and finally let's check the original arguments: {}", args.join(" "));
+}
+
+
+fn reverse(strings: &Vec<String>) -> Vec<String> {
+    let mut newvec = strings.to_vec();
+    newvec.reverse();
+    newvec
 }
