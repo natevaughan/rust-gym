050ccdaaf5755c300d250d189bb578b316ccb549
Nate Vaughan <nate.vaughan@protonmail.com>
2023-12-31 21:29:38 -0600

    Add enum for create and read

--- apples/src/main.rs
+++ apples/src/main.rs
@@ -1,11 +1,18 @@
 #[allow(warnings, unused)]
 mod prisma;
- 
+
+use std::env;
 use prisma::{PrismaClient, inventory};
 use prisma_client_rust::NewClientError;
  
 #[tokio::main]
 async fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Usage: ./apples <create|read> <name> <color>");
+    }
+
+    let action = parse_action(args[1].to_owned()).expect("Please select either \"create\" or \"read\" for the first agrument");
     let client: Result<PrismaClient, NewClientError> = PrismaClient::_builder().build().await;
 
     let stuff: Vec<inventory::Data> = client.expect("REASON")
@@ -17,5 +24,19 @@ async fn main() {
     .await
     .unwrap();
 
-    println!("{:?}", stuff)
+    println!("{:?} {:?}", action, stuff)
+}
+
+#[derive(Debug)]
+enum Action {
+    CREATE,
+    READ
+}
+
+fn parse_action(action: String) -> Option<Action> {
+    return match action.to_lowercase().as_ref() {
+        "create" => Some(Action::CREATE),
+        "read" => Some(Action::READ),
+        _ => None
+    }
 }
