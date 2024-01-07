285f5d3e39c1d7f436b798956c3019df2af60487
Nate Vaughan <nate.vaughan@protonmail.com>
2024-01-07 14:06:48 -0600

    primitives

---
+++ primates/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let ape = 0b111101;
+    println!("The chimpanzee is {} years old", ape);
+    let orang = 0x3b3;
+    println!("The orangutan has {} sticks in his collection", orang)
+    
+}
