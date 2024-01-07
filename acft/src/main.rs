6c3219e351dbda3cfc17adeb1126b8c11e695514
Nate Vaughan <nate.vaughan@protonmail.com>
2024-01-07 09:41:47 -0600

    fmt::Display for Vec, write!, and ? operator

--- acft/src/main.rs
+++ acft/src/main.rs
@@ -7,7 +7,17 @@ fn main() {
         model_name: "737".to_string(),
         model_variant: "MAX8".to_string()
     };
-    println!("This is a {0}, but if ATC asks for type, say {0:?}", b737_800)
+
+    let be36 = Aircraft {
+        manufacturer: "Beechcraft".to_string(),
+        model_shorthand: "BE36".to_string(),
+        model_name: "Bonanza".to_string(),
+        model_variant: "A36".to_string()
+    };
+    println!("This is a {0}, but if ATC asks for type, say {0:?}", b737_800);
+
+    let hangar = Hangar(Vec::from([b737_800, be36]));
+    println!("{}", hangar)
 }
 
 struct Aircraft {
@@ -26,5 +36,25 @@ impl fmt::Display for Aircraft {
 impl fmt::Debug for Aircraft {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.model_shorthand)
+    }
+}
+
+struct Hangar(Vec<Aircraft>);
+
+impl fmt::Display for Hangar {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        let mut i = 0;
+        for a in vec {
+            write!(f, "{:?}", a)?;
+            i += 1;
+            if i != vec.len() {
+                write!(f, ", ")?;
+            }
+        }
+        write!(f, "]")
     }
 }
