3463cf550cb217bbc7d1527dd535df401f96c993
Nate Vaughan <nate.vaughan@protonmail.com>
2024-01-07 09:53:50 -0600

    Use static str

--- grocery-list/src/main.rs
+++ grocery-list/src/main.rs
@@ -1,11 +1,11 @@
 use std::fmt;
 
 fn main() {
-    let a = Item(1, "stick".to_string(), "Butter".to_string());
+    let a = Item(1, "stick", "Butter");
     println!("{}", a)
 }
 
-struct Item(u8, String, String);
+struct Item(u8, &'static str, &'static str);
 
 impl fmt::Display for Item {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
