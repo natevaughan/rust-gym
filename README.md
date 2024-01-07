d901e6e1d3180b0553dc456a926f3c2f4bea38e1
Nate Vaughan <nate.vaughan@protonmail.com>
2024-01-07 10:15:29 -0600

    add GroceryList

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # rust-gym
 
-Fun little tasks to learn rust
+Fun little tasks to learn rust.
+
+## Questions
+
+Questions I'm exploring for further reading and exploration:
+ - Why does the borrow checker check for borrows of immutable data?
+ - What are the apostrophes for `&str` in structs e.g. `&'static str`?
+ - Where are different kinds of strings stored `String`, `&str`, `&'static str`? Heap, stack, ...?
+ - Why do some impls reference `&self`` and others `self`?
+
+ ## Notes
+
+ Currently on p20 of `rust-by-example.pdf`
--- grocery-list/src/main.rs
+++ grocery-list/src/main.rs
@@ -2,7 +2,10 @@ use std::fmt;
 
 fn main() {
     let a = Item(1, "stick", "Butter");
-    println!("{}", a)
+    let b = Item(1, "dozen", "eggs");
+    let c = Item(3, "whole", "Granny Smith apples");
+    let g = GroceryList {items: Vec::from([a, b, c])};
+    println!("{}", g)
 }
 
 struct Item(u8, &'static str, &'static str);
@@ -10,5 +13,26 @@ struct Item(u8, &'static str, &'static str);
 impl fmt::Display for Item {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} {} {}", self.0, self.1, self.2)
+    }
+}
+
+struct GroceryList {
+    items: Vec<Item>,
+}
+
+
+impl GroceryList {
+    fn count(&self) -> u8 {
+        return self.items.iter().map(|n| n.0).sum::<u8>()
+    }
+}
+
+impl fmt::Display for GroceryList {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Things to buy:\n")?;
+        for item in &self.items {
+            write!(f, "{}\n", item)?;
+        }
+        write!(f, "({} items total)", &self.count())
     }
 }
