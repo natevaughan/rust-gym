3c85698f83c92eb8954dc3a84c92a07342027c9b
Nate Vaughan <nate.vaughan@protonmail.com>
2024-03-08 16:01:19 -0600

    I ate a very good muffin just now

--- vecs_and_options/src/main.rs
+++ vecs_and_options/src/main.rs
@@ -2,7 +2,11 @@ fn main() {
 	let foo = vec![Cat{name: "Mango".to_string(), age: 11}];
 	println!("{:?}", foo);
 	for i in 0..5 {
-		foo.get(i).map(|c| println!("{} is {}", c.name, c.age));
+		let el = foo.get(i);
+		match el {
+			None => println!("No element at {}", i),
+			Some(c) => println!("{} is {}", c.name, c.age),
+		}
 	}
 }
 
