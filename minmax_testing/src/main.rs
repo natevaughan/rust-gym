8c1b1bcabc42d8f6578184aa69a7187cacf79782
Nate Vaughan <nate.vaughan@protonmail.com>
2024-03-11 11:39:45 -0500

    more verbose but efficent iteration, use i64

--- minmax_testing/src/main.rs
+++ minmax_testing/src/main.rs
@@ -7,10 +7,21 @@ fn print_min_max_sum(arr: &[i32]) {
 	println!("min: {}, max: {}", min, max)
 }
 
-fn min_max_sum(arr: &[i32]) -> (i32, i32) {
-	let mut min = arr.iter().min().unwrap();
-	let mut max = arr.iter().max().unwrap();
-	let sum: i32 = arr.iter().sum();
+fn min_max_sum(arr: &[i32]) -> (i64, i64) {
+	let first: i64 = arr[0].into();
+	let mut min = first;
+	let mut max = first;
+	let mut sum = first;
+	for i in 1..arr.len() {
+		let el: i64 = arr[i].into();
+		sum += el;
+		if el < min {
+			min = el
+		}
+		if el > max {
+			max = el
+		}
+	}
 	(sum - max, sum - min)
 }
 
@@ -22,7 +33,7 @@ mod tests {
 	fn test_min_max_sum_1() {
 		let test_arr: [i32; 5]  = [1, 2, 3, 4, 5];
 		let (min, max) = min_max_sum(&test_arr);
-		assert_eq!(10i32, min);
-		assert_eq!(14i32, max);
+		assert_eq!(10i64, min);
+		assert_eq!(14i64, max);
 	}
 }
