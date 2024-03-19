ee563519d3ef51bcc61ec030cab81e229f9626f0
Nate Vaughan <nate.vaughan@protonmail.com>
2024-03-18 22:31:53 -0500

    Example of a function that can use axum and run on aws lambda

--- README.md
+++ README.md
@@ -6,15 +6,21 @@ Fun little tasks to learn rust.
 
 Questions I'm exploring for further reading and exploration:
  - Why does the borrow checker check for borrows of immutable data?
+    - Borrows of immutable data are fine. Moves of immutable data are fine. It's borrows after moves that the compiler doesn't allow. One owner, one compile-checked drop.
  - What are the apostrophes for `&str` in structs e.g. `&'static str`?
  - Where are different kinds of strings stored `String`, `&str`, `&'static str`? Heap, stack, ...?
+    - `String` -> heap and ownership is checked
  - Why do some impls reference `&self`` and others `self`?
 
 ## Checklist
 
- - ✅ arr.iter() and iter.map()
- - ✅ num.abs()
+ - ✅ `arr.iter()` and `iter.map()`
+ - ✅ number basics like `num.abs()`, `num_str.parse::<i32>().unwrap()`
+ - ✅ tuple structs and C structs
+ - ✅ `Result`, `unwrap()`, `is_ok()`, and `is_err()`
+ - ✅ functions as first-class citizens, passing `fn` to `.map`
+ - ✅ borrowing vecs `for el in &v` and consuming an iterator `v.into_iter()`
 
 ## Notes
 
-Currently on p20 of `rust-by-example.pdf`
+p28 of `rust-by-example.pdf`
--- greeter-lambda/src/main.rs
+++ greeter-lambda/src/main.rs
@@ -1,69 +1,38 @@
-// use lambda_runtime::{service_fn, Error, LambdaEvent};
+use anyhow::Result;
+use axum::{routing::get, Json, Router};
+use lambda_http::{
+    http::status::StatusCode, service_fn, tower::ServiceExt, Error, IntoResponse, Request, Service,
+};
 use serde::Deserialize;
-use serde_json;
-// use std::collections::HashMap;
-use lambda_http::{service_fn, Error, RequestExt, IntoResponse, Request, Body, http::status::StatusCode};
-use anyhow::{Result};
 
+/**
+ * Uses axum for routing but lambda_http for accepting web requests
+ */
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    println!("Firing up the old lambda!!!!");
-    lambda_http::run(service_fn(hello)).await?;
+    println!("Firing up the lambda!!");
+    lambda_http::run(service_fn(handler)).await?;
     Ok(())
 }
 
-// anyhow: replace .expect("") with .context("")?
+async fn get_root() {}
 
-async fn hello(
-    request: Request
-) -> Result<impl IntoResponse> {
-    let _context = request.lambda_context_ref();
-    println!("request info: {:?}", request);
-    println!("URI: {:?}", request.uri());
-    println!("request body: {:?}", request.body());
-    let body = request.body();
-    let res = match body {
-        Body::Text(some_text) => {
-            let my_req: MyRequest = serde_json::from_str(some_text).expect("Error nate is dumb and doesn't know how to make better errors");
-            (StatusCode::OK, format!(
-                "hello {}",
-                my_req.name
-            ))
-        },
-        Body::Binary(_) => (StatusCode::NOT_FOUND, String::from("Not found")),
-        Body::Empty => (StatusCode::BAD_REQUEST, String::from("Fuck you")),
-    };
-    Ok(res)
+async fn post_root(Json(payload): Json<MyRequest>) -> (StatusCode, String) {
+    println!("request body: {:?}", payload);
+    (StatusCode::OK, format!("Nice to meet you {}", payload.name))
+}
+
+async fn delete_root() {}
+
+async fn handler(request: Request) -> Result<impl IntoResponse> {
+    let mut router =
+        Router::new().route("/axum", get(get_root).post(post_root).delete(delete_root));
+    let response = router.as_service().ready().await?.call(request).await?;
+
+    Ok(response)
 }
 
 #[derive(Deserialize, Debug)]
 struct MyRequest {
     name: String,
 }
-
-// #[derive(Serialize, Debug)]
-// struct Response {
-//     uuid: String,
-//     greeting: String,
-//     author_id: u64
-// }
-
-// pub(crate) async fn handle_post(event: LambdaEvent<String>) -> Result<Response, Error> {
-//     println!("{:?}", event.payload);
-//     let resp = Response {
-//         uuid: "213".to_string(),
-//         greeting: format!("Hello, {:?}", event),
-//         author_id: 42,
-//     };
-//     Ok(resp)
-// }
-
-// #[tokio::main]
-// async fn main() -> Result<(), Error> {
-//     println!("Greeting requested........");
-//     tracing_subscriber::fmt()
-//         .init();
-//     let func = service_fn(handle_post);
-//     lambda_runtime::run(func).await?;
-//     Ok(())
-// }
